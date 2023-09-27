ad29ad425ae6bb2752400cde547f2af878b2c63f
Vinicius Sartini <sartini.vinicius@outlook.com>
2023-09-26 21:50:48 -0300

    Retrabalho na lógica do script, add botão limpar

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,22 @@ const formulario = document.getElementById('formulario');
 
 formulario.addEventListener('submit', function(event) {
     event.preventDefault();
+
     const campoA = document.getElementById('campo-a');
     const campoB = document.getElementById('campo-b');
-    if (campoB.value > campoA.value) {
-        alert("Deu bom, meu padrin!");
+    const mensagemValida = `Formulário válido. Campo B: ${campoB.value} é maior que o Campo A: ${campoA.value}.`;
+    const mensagemInvalida = `Formulário inválido. Campo B: ${campoB.value} é menor ou igual ao Campo A: ${campoA.value}.`;
+
+    if (Number(campoB.value) > Number(campoA.value)) {
+        document.getElementById('mensagem').innerHTML = mensagemValida;
+        document.getElementById('mensagem').style.backgroundColor = "Green";
     } else {
-        alert("Deu ruim, meu padrin!");
+        document.getElementById('mensagem').innerHTML = mensagemInvalida;
+        document.getElementById('mensagem').style.backgroundColor = "Red";
     }
+})
+
+formulario.addEventListener('reset', function(event) {
+    document.getElementById('mensagem').innerHTML = "";
+    document.getElementById('mensagem').style.backgroundColor = "transparent";
 })
