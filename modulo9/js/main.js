76108531231c140358dcc4e7a8812425df17a0ff
Vinicius Sartini <sartini.vinicius@outlook.com>
2023-10-08 02:15:46 -0300

    Algumas modificações no css e js

---
+++ modulo9/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    $(".titulo-menu-cabecalho").on("mouseover mouseout", function() {
+        $(".lista-menu").slideToggle(500);
+
+    });
+
+    $(".menu-lateral").click(function() {
+
+    });
+
+});
